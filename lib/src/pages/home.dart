807abb28c4e08ccdab06da044bf68133e9f6f139
Levi Lesches <levilesches@gmail.com>
2021-07-22 00:23:28 -0400

    Configured HomePage to use Projects data instead of dummy data

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,21 +1,17 @@
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
 
-final List<String> names = [
-	"Gabrielle Amar",
-	"Danielle Bendavid",
-	"Simone Finkelstein",
-	"Arielle Hadad",
-	"Hannah Hoff",
-	"Mariah Hubert",
-	"Moselle Kleiner",
-	"Ilan Kogan",
-	"Laura Rosen",
-	"Sabrina Schochet",
-	"Olivia Schwartz",
-	"Lily Shams",
-];
+import "package:artist_project/models.dart";
+import "package:artist_project/data.dart";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+	@override
+	HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+	int season = 0;
+
 	Widget middleBold(
 		TextStyle? basic, 
 		String beginning, 
@@ -39,64 +35,84 @@ class HomePage extends StatelessWidget {
 	));
 
 	@override
-	Widget build(BuildContext context) => Scaffold(
-		backgroundColor: Colors.white,
-		body: Center(
-			child: FractionallySizedBox(
-				widthFactor: 3/4,
-				// A ListView can't center its children horizontally, so we use Column instead
-				child: SingleChildScrollView(child: Column(
-					children: [
-						Text("The Ramaz Upper School", style: Theme.of(context).textTheme.headline5),
-						const SizedBox(height: 8),
-						middleBold(
-							Theme.of(context).textTheme.headline2?.copyWith(color: Colors.black),
-							"THE ", "ARTIST", " PROJECT"
-						),
-						const SizedBox(height: 8),
-						Text("What artists see when they look at The Met", style: Theme.of(context).textTheme.subtitle1),
+	Widget build(BuildContext context) => ChangeNotifierProvider<Projects>.value(
+		value: Models.instance.projects,
+		builder: (_, __) => Scaffold(
+			backgroundColor: Colors.white,
+			body: Center(
+				child: FractionallySizedBox(
+					widthFactor: 3/4,
+					// A ListView can't center its children horizontally, so we use a 
+					// SingleChildScrollView of a Column instead.
+					child: SingleChildScrollView(child: Column(
+						children: [
+							Text(
+								"The Ramaz Upper School", 
+								style: Theme.of(context).textTheme.headline5,
+							),
+							const SizedBox(height: 8),
+							middleBold(
+								Theme.of(context).textTheme.headline2?.copyWith(color: Colors.black),
+								"THE ", "ARTIST", " PROJECT"
+							),
+							const SizedBox(height: 8),
+							Text(
+								"What artists see when they look at The Met", 
+								style: Theme.of(context).textTheme.subtitle1,
+							),
 
-						const SizedBox(height: 72),
-						const Divider(),
-						const SizedBox(height: 16),
+							const SizedBox(height: 72),
+							const Divider(),
+							const SizedBox(height: 16),
 
-						Row(
-							mainAxisAlignment: MainAxisAlignment.spaceBetween,
-							children: [
-								const SizedBox(width: 8),
-								NamesList(names.sublist(0, 6)),
-								NamesList(names.sublist(6)),
-								const SizedBox(width: 4),
-							],
-						),
+							NamesList(season: season),
 
-						const SizedBox(height: 4),
-						const Divider(),
-						const SizedBox(height: 24),
+							const SizedBox(height: 4),
+							const Divider(),
+							const SizedBox(height: 24),
 
-						middleBold(
-							Theme.of(context).textTheme.subtitle1,
-							"ABOUT THE ", "ARTIST", " PROJECT"
-						)
-					]
-				))
+							middleBold(
+								Theme.of(context).textTheme.subtitle1,
+								"ABOUT THE ", "ARTIST", " PROJECT"
+							)
+						]
+					))
+				)
 			)
 		)
 	);
 }
 
 class NamesList extends StatelessWidget {
-	final List<String> names;
-	const NamesList(this.names);
+	final int season;
+	const NamesList({required this.season});
 
 	@override
-	Widget build(BuildContext context) => Column(
-		children: [
-			for (final String name in names) ...[
-				Text(name, style: Theme.of(context).textTheme.headline5?.copyWith(color: Colors.red, fontWeight: FontWeight.bold)),
-				const SizedBox(height: 12),
-			]
-		]
+	Widget build(BuildContext context) => Consumer(
+		builder: (_, Projects model, __) {
+			final List<Project> seasonProjects = model.projects [season];
+			final int middle = seasonProjects.length ~/ 2;
+			final List<Project> left = seasonProjects.sublist(0, middle);
+			final List<Project> right = seasonProjects.sublist(middle);
+			return Row(
+				mainAxisAlignment: MainAxisAlignment.spaceBetween,
+				children: [
+					const SizedBox(width: 8),
+					for (List<Project> projects in [left, right]) Column(
+						children: [
+							for (final Project project in projects) ...[
+								Text(
+									project.student.name, 
+									style: Theme.of(context).textTheme.headline5
+										?.copyWith(color: Colors.red, fontWeight: FontWeight.bold)
+								),
+								const SizedBox(height: 12),
+							]
+						],
+					),
+					const SizedBox(width: 4),
+				],
+			);
+		}
 	);
 }
-
