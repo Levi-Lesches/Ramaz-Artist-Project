26a093a85c21888127e1458d976ec2865c129c9d
Levi Lesches <levilesches@gmail.com>
2021-07-25 17:43:29 -0400

    Documented Project

--- lib/src/data/project.dart
+++ lib/src/data/project.dart
@@ -1,16 +1,35 @@
 import "episode_art.dart";
 import "student.dart";
 
+/// A project where students discuss famous artwork.
 class Project {
+	/// Details about various artwork seen in the video.
 	final List<EpisodeArt> episodeArt;
+
+	/// The student who made this project. 
 	final Student student;
 
+	/// The season this project belongs to.
+	/// 
+	/// Each "season" is a new year of seniors. 
 	final int season;
+
+	/// The artist this project focuses on.
+	/// 
+	/// Some artwork's artists are unknown, and some projects focus on multiple
+	/// pieces, or pieces where there is no one artist, so this field is nullable.
 	final String? artist;
+
+	/// The specific piece of art this project focuses on.
 	final String artwork;
+
+	/// An inspirational quote from the student about the piece. 
 	final String quote;
+
+	/// A YouTube ID to the video this student made. 
 	final String videoLink;
 
+	/// Creates a [Project] with the given fields.
 	const Project({
 		required this.episodeArt,
 		required this.student,
@@ -21,6 +40,7 @@ class Project {
 		required this.videoLink,
 	});
 
+	/// Creates a [Project] from a JSON object.
 	Project.fromJson(Map json) : 
 		episodeArt = EpisodeArt.fromList(json ["episodeArt"].cast<Map>()),
 		student = Student.fromJson(json ["student"]),
@@ -30,6 +50,7 @@ class Project {
 		quote = json ["quote"],
 		videoLink = json ["videoLink"];
 
+	/// Converts this object to JSON.
 	Map<String, dynamic> toJson() => {
 		"episodeArt": episodeArt.map((art) => art.toJson()),
 		"student": student.toJson(),
